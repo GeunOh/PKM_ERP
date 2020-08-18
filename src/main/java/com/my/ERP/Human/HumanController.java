4363a1a52c881ddec2d209f39a4bf19b1bd670a9
sanghoo <sanghoo96@naver.com>
2020-08-18 20:00:34 +0900

    css수정 및 급여페이지-1

--- src/main/java/com/my/ERP/Human/HumanController.java
+++ src/main/java/com/my/ERP/Human/HumanController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HumanController {
 	
-	
+	// 인사기본관리
 	@RequestMapping("HumanManager")
 	public String HumanMain() {
 		return "HumanManager";
+	}
+	
+	// 급여관리
+	@RequestMapping("SalaryManager")
+	public String SalaryMain() {
+		return "SalaryManager";
 	}
 }
